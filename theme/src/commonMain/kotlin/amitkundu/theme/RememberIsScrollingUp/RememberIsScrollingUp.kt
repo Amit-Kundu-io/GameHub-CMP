a8767e98ac5d525387d0439274cfb239ae227f9e
Amit Kundu <amitkundu.developer@gmail.com>
2026-01-06 00:09:40 +0530

    fixed scroll up problem

--- theme/src/commonMain/kotlin/amitkundu/theme/RememberIsScrollingUp/RememberIsScrollingUp.kt
+++ theme/src/commonMain/kotlin/amitkundu/theme/RememberIsScrollingUp/RememberIsScrollingUp.kt
@@ -1,5 +1,6 @@
 package amitkundu.theme.RememberIsScrollingUp
 
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -11,29 +12,44 @@ import androidx.compose.runtime.snapshotFlow
 
 @Composable
 fun rememberIsScrollingUp(
-    listState: androidx.compose.foundation.lazy.LazyListState
+    listState: LazyListState
 ): Boolean {
 
-    var previousIndex by remember { mutableIntStateOf(0) }
-    var previousScrollOffset by remember { mutableIntStateOf(0) }
     var isScrollingUp by remember { mutableStateOf(true) }
+    var lastIndex by remember { mutableIntStateOf(0) }
+    var lastOffset by remember { mutableIntStateOf(0) }
+
+    //  NEW: track real user scroll
+    var hasUserScrolled by remember { mutableStateOf(false) }
 
     LaunchedEffect(listState) {
         snapshotFlow {
             listState.firstVisibleItemIndex to
                     listState.firstVisibleItemScrollOffset
         }.collect { (index, offset) ->
+
+            // Ignore restore / initial emissions
+            if (!hasUserScrolled) {
+                if (index != 0 || offset != 0) {
+                    hasUserScrolled = true
+                }
+                lastIndex = index
+                lastOffset = offset
+                return@collect
+            }
+
             isScrollingUp =
-                if (index == previousIndex) {
-                    offset < previousScrollOffset
+                if (index == lastIndex) {
+                    offset < lastOffset
                 } else {
-                    index < previousIndex
+                    index < lastIndex
                 }
 
-            previousIndex = index
-            previousScrollOffset = offset
+            lastIndex = index
+            lastOffset = offset
         }
     }
 
     return isScrollingUp
 }
+
